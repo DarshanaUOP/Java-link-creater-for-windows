2915b0953c3a273fc88aba424423312189481b08
Darshana Ariyarathna <darshana.uop@gmail.com>
2021-05-14 23:34:34 +0530

    created basic functionality UI

--- src/main/java/CreateLinks.java
+++ src/main/java/CreateLinks.java
@@ -1,6 +1,10 @@
 import com.deltaApps.assessories.windows.linkcreator.FrontEnd;
 
 public class CreateLinks {
+    /**
+     * This project will create links for your window file system.
+     * @see 'https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/mklink'
+     */
     public static void main(String[] args) {
 //        LinkCreator linkCreator1 = new LinkCreator("C:\\Users\\Darshana Ariyarathna\\Downloads","tests",'j',"G:\\Documents-old\\laravel");
         FrontEnd frontEnd = new FrontEnd();
--- src/main/java/com/deltaApps/assessories/windows/linkcreator/FrontEnd.java
+++ src/main/java/com/deltaApps/assessories/windows/linkcreator/FrontEnd.java
@@ -1,36 +1,148 @@
 package com.deltaApps.assessories.windows.linkcreator;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.io.IOException;
 
 public class FrontEnd extends JFrame {
 
-    private JLabel linkLabel,targetLabel;
+    private JLabel linkLabel,linkOutLabel,targetLabel,targetOutLabel;
     private JTextField linkFolderNameTxt;
     private JComboBox<String> linkTypeParam;
+    private JButton createButton;
+
+    private JPanel backPanel,linkPanel,targetPanel,processPanel;
+
+    private String linkDirectory;
+    private String targetDirectory;
+    private String linkFolder;
+    private char linkType;
+
+    LinkCreator linkCreator;
+
+    GridBagConstraints gbc = new GridBagConstraints();
 
     public FrontEnd(){
+        // Create file chooser.
+        JFileChooser fileChooser = new JFileChooser(this.getTreeLock().toString());
+        fileChooser.setDialogType(JFileChooser.OPEN_DIALOG);
+        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+        // JFrame layout settings
+        this.setLayout(new GridBagLayout());
+
+        // UI objects
 
         linkLabel = new JLabel("Click to select link location");
-
-        JFileChooser fileChooser = new JFileChooser(this.getTreeLock().toString());
-        fileChooser.setDialogType(JFileChooser.DIRECTORIES_ONLY);
-
         linkLabel.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                fileChooser.showOpenDialog(FrontEnd.this);
-                String dir = String.valueOf(fileChooser.getCurrentDirectory());
-                System.out.println(dir);
+                int fileName =  fileChooser.showOpenDialog(FrontEnd.this);
+
+                if (fileName == JFileChooser.APPROVE_OPTION){
+                    try {
+                        linkDirectory = fileChooser.getSelectedFile().getCanonicalPath();
+                        System.out.println("OPEN : " + linkDirectory);
+                    } catch (IOException ioException) {
+                        ioException.printStackTrace();
+                    }
+                    linkOutLabel.setText(linkDirectory);
+                }
             }
         });
-        add(linkLabel);
 
         String[] choices = { "Directory Junction", "Symbolic link","Hard link"};
         JComboBox<String> linkTypeParam = new JComboBox<String>(choices);
-//        add(linkTypeParam);
 
+        linkOutLabel = new JLabel("You are not selected a link directory");
+        linkFolderNameTxt = new JTextField("new folder");
+
+        backPanel = new JPanel(new GridBagLayout());
+
+        linkPanel = new JPanel(new BorderLayout());
+        Border linkPanelBorder = BorderFactory.createTitledBorder("Link");
+        linkPanel.setBorder(linkPanelBorder);
+        linkPanel.add(linkLabel,BorderLayout.WEST);
+        linkPanel.add(linkTypeParam,BorderLayout.EAST);
+        linkPanel.add(linkFolderNameTxt,BorderLayout.CENTER);
+        linkPanel.add(linkOutLabel,BorderLayout.SOUTH);
+
+        targetOutLabel = new JLabel("You are not selected a target directory");
+        targetLabel = new JLabel("Select the target");
+        targetLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int fileName =  fileChooser.showOpenDialog(FrontEnd.this);
+
+                if (fileName == JFileChooser.APPROVE_OPTION){
+                    try {
+                        targetDirectory = fileChooser.getSelectedFile().getCanonicalPath();
+                        System.out.println("OPEN : " + targetDirectory);
+                    } catch (IOException ioException) {
+                        ioException.printStackTrace();
+                    }
+                    targetOutLabel.setText(targetDirectory);
+                }
+            }
+        });
+        targetPanel = new JPanel(new BorderLayout());
+        Border targetPanelBorder = BorderFactory.createTitledBorder("Target");
+        targetPanel.setBorder(targetPanelBorder);
+        targetPanel.add(targetLabel,BorderLayout.NORTH);
+        targetPanel.add(targetOutLabel,BorderLayout.SOUTH);
+
+        createButton = new JButton("Proceed");
+        createButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                linkFolder = linkFolderNameTxt.getText();
+
+                int selectedIndex = linkTypeParam.getSelectedIndex();
+                switch (selectedIndex){
+                    case 1:
+                        linkType = 'h';
+                        break;
+                    case 2:
+                        linkType = 'd';
+                        break;
+                    default:
+                        linkType = 'j';
+                }
+
+                linkCreator = new LinkCreator(linkDirectory , linkFolder, linkType , targetDirectory);
+            }
+        });
+
+        processPanel = new JPanel(new BorderLayout());
+        Border processPanelBorder = BorderFactory.createTitledBorder("process");
+        processPanel.setBorder(processPanelBorder);
+        processPanel.add(createButton);
+
+        // Adding components
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 2;
+        gbc.gridheight = 1;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        backPanel.add(linkPanel,gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        backPanel.add(targetPanel,gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        backPanel.add(processPanel,gbc);
+
+        add(backPanel);
+
+        // JFrame settings
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationByPlatform(true);
         setSize(500,400);
